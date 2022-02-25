b36e48e58140ebc76264379273d7cb3279635ff9
Geert JM Vanderkelen <geert@vanderkelen.org>
2022-02-25 08:00:32 +0000

    Refactor setup.sh creating setup_basic_config_file function

--- setup.sh
+++ setup.sh
@@ -33,7 +33,50 @@ backup ()
 		mkdir -p ${BACKUP_DIR}
 	fi
 
+	echo "  backing up: $1 -> ${BACKUP_DIR}/$2"
 	mv $1 ${BACKUP_DIR}/$2
+}
+
+setup_basic_config_file ()
+{
+	name="${1}"
+	src="${HD}/${2}"
+	dst="${HOME}/${3}"
+	backup_file="${3}"
+
+	if [ "${src}" == "" ]; then
+		echo "(panic) source not provided"
+		exit 1
+	fi
+
+	if [ "${dst}" == "" ]; then
+		echo "(panic) destination not provided"
+		exit 1
+	fi
+
+	if [ ! -e "${src}" ]; then
+		echo "(panic) source '${src}' does not exist"
+		exit 2
+	fi
+
+	if [ ! -f "${src}" ]; then
+		echo "(panic) source '${src}' must be regular file"
+		exit 2
+	fi
+
+	if [ -L "${dst}" ]; then
+		echo "Skipping ${name}."
+		return
+	fi
+
+	echo "Set up ${name} .. "
+	backup ${dst} ${backup_file}
+
+	echo "  link ${src} -> ${dst}" 
+	ln -s ${src} ${dst}
+	chmod 0600 ${dst}
+
+	echo "  done!"
 }
 
 ##
@@ -45,7 +88,7 @@ setup_ssh () {
 		return
 	fi
 
-	echo -n "Set up SSH .. "
+	echo "Set up SSH .. "
 
 	if [ -f ${HOME}/.ssh/authorized_keys ]; then
 		mkdir -p ${BACKUP_DIR}/ssh
@@ -63,47 +106,11 @@ setup_ssh () {
 	ln -s ${HD}/ssh/authorized_keys ${HOME}/.ssh/authorized_keys
 	chmod 0600 ${HD}/ssh/authorized_keys
 
-	echo "done!"
-}
-
-###
-### ZSH
-###
-setup_zsh ()
-{
-	if [ -L .zshrc ]; then
-		echo "Skipping ZSH."
-		return
-	fi
-
-	echo -n "Set up ZSH .. "
-	backup ${HOME}/.zshrc zshrc
-
-	ln -s ${HD}/zshrc ${HOME}/.zshrc
-	chmod 0600 ${HD}/zshrc
-
-	echo "done!"
-}
-
-###
-### Git
-###
-setup_git ()
-{
-	if [ -L .gitconfig ]; then
-		echo "Skipping Git."
-		return
-	fi
-
-	echo -n "Set up Git .. "
-	backup ${HOME}/.gitconfig gitconfig
-
-	ln -s ${HD}/gitconfig ${HOME}/.gitconfig
-	chmod 0600 ${HD}/gitconfig
-
-	echo "done!"
+	echo "  done!"
 }
 
 setup_ssh
-setup_zsh
-setup_git
+setup_basic_config_file "ZSH" "zshrc" ".zshrc"
+setup_basic_config_file "Git" "gitconfig" ".gitconfig"
+setup_basic_config_file "VIm" "vimrc" ".vimrc"
+
